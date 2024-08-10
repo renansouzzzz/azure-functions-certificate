155b4e4768e7bea0471c1f713b12be9cb8bc241d
renansouzzzz <renansouza.oliv@outlook.com>
2024-08-10 13:09:18 -0300

    test

--- CertificateFunction.cs
+++ CertificateFunction.cs
@@ -4,21 +4,46 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using CertificateFunction.Services;
 
+namespace CertificateFunction
 {
+    public class CertificateFunction
     {
+        private readonly ServiceCertificate _certificateService;
 
+        public CertificateFunction(ServiceCertificate certificateService)
         {
             _certificateService = certificateService;
         }
 
         [FunctionName("GetCertificateFunction")]
         public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethods.Get), Route = "certificate/{url}")] HttpRequest req,
+            string url,
             ILogger log)
         {
+            if (string.IsNullOrEmpty(url))
+            {
+                return new BadRequestObjectResult("URL is required");
+            }
 
+            var certificate = await _certificateService.GetServerCertificateAsync(url);
 
+            if (certificate == null)
+            {
+                return new NotFoundObjectResult("No certificate found");
+            }
 
+            return new OkObjectResult(certificate);
+        }
+
+        [FunctionName("TestFunction")]
+        public IActionResult RunTest(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "test")] HttpRequest req,
+        ILogger log)
+        {
+            return new OkObjectResult("Function is working");
         }
     }
 }
